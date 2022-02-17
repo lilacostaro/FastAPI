4dcc5ad351b1262156fac7754c7e309b38f1c961
lilacostaro <lila.costa.ro@gmail.com>
2022-02-17 15:29:55 -0300

    query parameter type conversion

--- user_guide/opt_params.py
+++ user_guide/opt_params.py
@@ -3,7 +3,14 @@ from fastapi import FastAPI
 app = FastAPI()
 
 @app.get('/items/{item_id}')
-async def read_item(item_id: str, q: str | None = None):
+async def read_item(item_id: str, q: str | None = None, short: bool = False):
+    item = {'item_id': item_id}
     if q:
-        return {'item_id': item_id, 'q': q}
-    return {'item_id': item_id}
+        item.update(
+            {'q': q}
+        )
+    if not short:
+        item.update(
+            {'description': 'This is an amazing item tha has a long description'}
+        )
+    return item
