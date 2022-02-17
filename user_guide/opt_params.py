502874e88f93de2f2a6dff12de56ead36fad4d2d
lilacostaro <lila.costa.ro@gmail.com>
2022-02-17 16:14:06 -0300

    add functions item_required and optional_required

--- user_guide/opt_params.py
+++ user_guide/opt_params.py
@@ -29,3 +29,15 @@ async def read_user_item(user_id: int, item_id: str, q: str | None = None, short
             {'description': 'This is an amazing item tha has a long description'}
         )
     return item
+
+# With required paramenters.
+@app.get('/required/{item_id}')
+async def item_required(item_id: str, needy: str):
+    item = {'item_id': item_id, 'needy': needy}
+    return item
+
+# with required, non-required and optional parameters
+@app.get('/optional/{item_id}')
+async def optional_required(item_id: str, needy: str, skip: int = 0, limit: int | None = None):
+    item = {'item_id': item_id, 'needy': needy, 'skip': skip, 'limit': limit}
+    return item
