e5dde10315a78527dc5c3efa62fcf3b00e301e11
lilacostaro <lila.costa.ro@gmail.com>
2022-02-17 15:57:01 -0300

    add function read_user_item

--- user_guide/opt_params.py
+++ user_guide/opt_params.py
@@ -2,9 +2,24 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+# Query with no required parameters.
 @app.get('/items/{item_id}')
 async def read_item(item_id: str, q: str | None = None, short: bool = False):
     item = {'item_id': item_id}
+    if q:
+        item.update(
+            {'q': q}
+        )
+    if not short:
+        item.update(
+            {'description': 'This is an amazing item tha has a long description'}
+        )
+    return item
+
+# Multiple path and query parameters
+@app.get('/users/{user_id}/items/{item_id}')
+async def read_user_item(user_id: int, item_id: str, q: str | None = None, short: bool = False):
+    item = {'item_id': item_id, 'owner_id': user_id}
     if q:
         item.update(
             {'q': q}
